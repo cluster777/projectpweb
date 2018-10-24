79098597a60bb25c4fc7ecca53dd1269dd177983
cluster777 <42055002+cluster777@users.noreply.github.com>
2018-10-24 13:40:44 +0700

    template untuk page donasi

---
+++ main_donation temp.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Portfolio Item - Start Bootstrap Template</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+  </head>
+
+  <body>
+
+    <!-- Navigation 
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">Start Bootstrap</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Services</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+	-->
+    <!-- Page Content -->
+    <div class="container">
+
+      <!-- Portfolio Item Heading -->
+      <h1 class="my-4">Page title
+      </h1>
+
+      <!-- Portfolio Item Row -->
+      <div class="row">
+
+        <div class="col-lg-7 col-sm-8 ">
+          <img src="http://placehold.it/600x400" class="img-fluid" alt="Responsive image">
+        </div>
+
+        <div class="col-lg-4 col-sm-6 ">
+          <h3 class="my-3">donation description</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.</p>
+          
+          <a href=#><h3>donate now</h3></a>
+        </div>
+
+      </div><br>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id iaculis metus. Morbi blandit, metus eget tristique consequat, mi mi luctus velit, ac finibus nibh diam luctus ligula. Ut venenatis pellentesque sapien. Suspendisse sollicitudin congue purus ac interdum. Vivamus luctus magna arcu, non ultricies nisi dignissim vitae. Sed rhoncus dictum consectetur. Mauris a arcu non felis luctus lacinia. Aliquam nibh sapien, molestie eget enim sed, hendrerit volutpat arcu. Donec eu nisl iaculis, feugiat nunc sit amet, commodo lorem. Vivamus dictum purus at ligula interdum molestie. Quisque placerat vestibulum sagittis.</p>
+
+		<p>Proin dolor quam, pharetra aliquet ipsum at, placerat imperdiet dolor. Maecenas venenatis odio porta, aliquet lacus at, euismod leo. Suspendisse sollicitudin tempor aliquet. Maecenas auctor tincidunt tellus et convallis. Donec magna felis, posuere id quam eleifend, luctus fringilla dui. Aliquam et lacus molestie dolor auctor tristique vel molestie orci. Ut vehicula risus ipsum, vel rhoncus nisl commodo ac. Praesent rhoncus ex est, quis tincidunt leo molestie vel. Maecenas ac diam et purus suscipit venenatis a in neque. Phasellus tempor vitae dolor iaculis suscipit. Curabitur malesuada magna sed nibh ullamcorper suscipit. Sed nec turpis quis ligula ultricies vestibulum vitae eget magna. Vestibulum nibh nunc, placerat ut libero quis, viverra dapibus mi. Nam quis tristique libero, sit amet tempor dui. Sed non fermentum nisl. Nunc fermentum purus magna, sed pharetra est porttitor a.</p>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Your Website 2018</p>
+      </div>
+      <!-- /.container -->
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
